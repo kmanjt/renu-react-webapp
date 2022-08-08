9d3c656da6ec0bd59964dff24aa638c47fa5ca1a
Kevin James T <95762894+kmanjt@users.noreply.github.com>
2022-08-08 21:15:24 +0100

    blogpage

--- frontend/src/components/pages/Blog.jsx
+++ frontend/src/components/pages/Blog.jsx
@@ -6,6 +6,17 @@ export default function Blog() {
     const [blogs, setBlogs]=useState([]);
     const [featuredBlog, setFeaturedBlog]=useState([]);
 
+    function fetchBlogs() {
+        axios
+        .get("/api/blog/")
+        .then(res => {
+            setBlogs(res.data);
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }    
+
     function fetchData() {
         axios
         .get("/api/blog/featured")
@@ -19,6 +30,7 @@ export default function Blog() {
             
     useEffect(() => {
         fetchData();
+        fetchBlogs();
         }
     , []);
 
@@ -29,6 +41,39 @@ export default function Blog() {
     };
 
     const getBlogs = () => {
+        let list = [];
+        let result = [];
+
+        blogs.map(blogPost => {
+            return list.push(
+    <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div className="col p-4 d-flex flex-column position-static">
+          <strong className="d-inline-block mb-2 text-primary">{blogPost.category}</strong>
+          <h3 className="mb-0">{blogPost.title}</h3>
+          <div className="mb-1 text-muted">{blogPost.month}</div>
+          <p className="card-text mb-auto">{blogPost.excerpt}</p>
+          <Link to={`/blog/${blogPost.slug}`} className="stretched-link">Continue reading</Link>
+        </div>
+        <div className="col-auto d-none d-lg-block">
+            <img width='200' height='250' src={blogPost.thumbnail} alt='thumbnail' />
+        </div>
+        </div>
+            );
+        })
+
+        for (let i=0; i < list.length; i += 2) {
+            result.push(
+                <div key={i} className='row mb-2'>
+                    <div className='col-md-6'>
+                        {list[i]}
+                    </div>
+                    <div className='col-md-6'>
+                        {list[i+1] ? list[i+1] : null}
+                    </div>
+                </div>
+            )
+        }
+        return result;
     };
 
 return (
@@ -51,7 +96,7 @@ return (
                 Continue reading...</Link></p>
     </div>
   </div>
-
+    {getBlogs()}
     </div>
     );
 }
