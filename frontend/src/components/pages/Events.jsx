174f01aca297208c8b0c191449c62267a0442f2f
Kevin James T <95762894+kmanjt@users.noreply.github.com>
2022-08-14 23:41:19 +0100

    calendar fix 2.0

--- frontend/src/components/pages/Events.jsx
+++ frontend/src/components/pages/Events.jsx
@@ -4,7 +4,7 @@ function Events() {
     return (
         <div className="embed-responsive">
             <h1>Events Header</h1>
-            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FDublin&src=ZW5hY3R1c2RjdUBnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uaXJpc2gjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%237986CB&color=%239E69AF&color=%23009688" scrolling="no"></iframe>
+            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FDublin&src=ZW5hY3R1c2RjdUBnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uaXJpc2gjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%237986CB&color=%239E69AF&color=%23009688" width="800" height="600" frameborder="0" scrolling="no"></iframe>
         </div>
     )
 }
