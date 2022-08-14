fc74017e55e21321f57c354b6742f962d0f1c031
Kevin James T <95762894+kmanjt@users.noreply.github.com>
2022-08-14 23:40:46 +0100

    calendar fix

--- frontend/src/components/pages/Events.jsx
+++ frontend/src/components/pages/Events.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 function Events() {
     return (
-        <div>
+        <div className="embed-responsive">
             <h1>Events Header</h1>
+            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FDublin&src=ZW5hY3R1c2RjdUBnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uaXJpc2gjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%237986CB&color=%239E69AF&color=%23009688" scrolling="no"></iframe>
         </div>
     )
 }
