b881d3979a0c1e45fd909d52d664e437364eeb46
Niamh <niamh.gowran2@mail.dcu.ie>
2022-08-17 00:07:14 +0100

    Merge

--- backend/renu/renu/settings.py
+++ backend/renu/renu/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test',
         'USER': 'root',
-        'PASSWORD': '3883',
+        'PASSWORD': '0305',
         'HOST': '127.0.0.1',
         'PORT':3306,
     }
