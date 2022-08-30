199f930bb23121aaa99a36dbd1a3d4a03ba91de8
Niamh <niamh.gowran2@mail.dcu.ie>
2022-08-30 14:02:21 +0100

    Style

--- frontend/src/components/pages/Events.jsx
+++ frontend/src/components/pages/Events.jsx
@@ -4,11 +4,13 @@ import '../baseStyle.css';
 function Events() {
     return (
         <div className='main-theme'>
-            <div className="embed-responsive text-center section rounded-3 p-4">
+            <div className="text-center section rounded-3 p-4">
                 <h1 className='display-5 fw-bold'>Our Upcoming Events</h1>
                 <p className='fs-4'>Add events directly to your google calendar</p>
                 <br></br>
-                <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FDublin&src=ZW5hY3R1c2RjdUBnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uaXJpc2gjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%237986CB&color=%239E69AF&color=%23009688" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+                <div className='embed-responsive'>
+                    <iframe class="embed-responsive-item" src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FDublin&src=ZW5hY3R1c2RjdUBnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uaXJpc2gjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%237986CB&color=%239E69AF&color=%23009688" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+                </div>
             </div>
         </div>
     )
